a90db595e99d4961d0e076b8a805fcf78c283c18
Kangmin Xie <kangmin.xie@gmail.com>
2020-08-16 12:40:17 -0500

    FIN-0001: initial setup of default files

---
+++ src/main/java/com/kangmin/app/MyFinanceWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kangmin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinanceWebServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(MyFinanceWebServiceApplication.class, args);
+    }
+}
